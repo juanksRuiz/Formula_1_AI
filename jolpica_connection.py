20a6e3db2ccccb662eebd9137636eb45771f40ce
Juan Camilo Ruiz <juankruizo10@gmail.com>
2024-11-13 14:21:54 -0800

    Logica incompleta para recuperar todos los datos de las ultimas rondas

--- jolpica_connection.py
+++ jolpica_connection.py
@@ -92,25 +92,49 @@ def get_round_results(season_data, round_id):
 
 
 # ----------------------------------------------------------------------------
+# Utility function
+def convert_time_to_timedelta(time_str):
+    """
+    Convert a string time in the format 'M:SS.SSS' to a pandas Timedelta.
+    
+    Parameters:
+    time_str (str): Time string in the format 'M:SS.SSS'
+    
+    Returns:
+    pandas.Timedelta: Timedelta representation of the input time string
+    """
+    # Se ignora en caso de que el campo venga vacio o con None por
+    # no haber clasificado
+    if pd.isna(time_str) or time_str.strip() == '':
+        return time_str
+        # return pd.Timedelta(0)
+    else:
+        parts = time_str.split(':')
+        minutes = int(parts[0])
+        seconds = float(parts[1])
+        
+        return pd.Timedelta(minutes=minutes, seconds=seconds)
+
+# ----------------------------------------------------------------------------
 # Ahora queremos preprocesar cada tabla de cada ronda  y crear las columnas
 # de mejora de Q1 a Q3
 def preprocess_round_data(round_df):
     # Paso 1: transformar las columnas de tiempos Qualis a time_delta
-    round_df['best_time_Q1'] = pd.to_timedelta('00:' + \
-                                               round_df['best_time_Q1'])
-    round_df['best_time_Q2'] = pd.to_timedelta('00:' + \
-                                               round_df['best_time_Q2'])
-    round_df['best_time_Q3'] = pd.to_timedelta('00:' + \
-                                               round_df['best_time_Q3'])
+    df = round_df.copy()
+    df['best_time_Q1'] = df['best_time_Q1'].apply(convert_time_to_timedelta)
+    df['best_time_Q2'] = df['best_time_Q2'].apply(convert_time_to_timedelta)
+    df['best_time_Q3'] = df['best_time_Q3'].apply(convert_time_to_timedelta)
     
     # Paso 2: calcular diferencia entre Qualis
-    round_df['time_diff_Q1_Q2_s'] = (round_df['best_time_Q2'] - \
-                                           round_df['best_time_Q1']).dt.\
+    df['time_diff_Q1_Q2_s'] = (df['best_time_Q2'] - \
+                                           df['best_time_Q1']).dt.\
                                     total_seconds()
                                     
-    round_df['time_diff_Q2_Q3_s'] = (round_df['best_time_Q3'] - \
-                                           round_df['best_time_Q2']).dt.\
+    df['time_diff_Q2_Q3_s'] = (df['best_time_Q3'] - \
+                                           df['best_time_Q2']).dt.\
                                     total_seconds()
                                     
-    round_df['time_diff_Q1_Q3_s'] = round_df['time_diff_Q1_Q2_s'] + \
-                                    round_df['time_diff_Q2_Q3_s']
+    df['time_diff_Q1_Q3_s'] = df['time_diff_Q1_Q2_s'] + \
+                                    df['time_diff_Q2_Q3_s']
+                                    
+    return df
--- main_f1.py
+++ main_f1.py
@@ -1,3 +1,35 @@
+# Module imports
+from jolpica_connection import *
+import pandas as pd
+
 # La idea de este archivo es probar el flujo de las funciones creadas
 # Para traer los datos de la API, preprocesarlos y tener una tabla final
 # consolidada con las distintas de funetes de datos
+
+# Cargue de librerias
+# PASO 1: definicion de parametros como la temporada y ronda de la cual
+# Se calculara los resultados con las 10 carreras anteriores.
+season_years = [2022, 2023]
+round_wdw = 10
+prediction_round = 11 # si es inferior a 10, crear flujo para las carreras
+# de la ronda anterior
+
+# PASO 2.1: obtener la tabla preprocesada de los tiempos de Qualis
+if prediction_round > round_wdw:
+    max_season = max(season_years)
+    print('Collecting data from season {temp}...'.format(temp=max_season))
+    season_data = get_season_qualifying_results(max_season)
+    print('Data collection from season {temp} successful!'.format(temp=max_season))
+    print('Concatenating data...')
+    for i in range(prediction_round-round_wdw, prediction_round):
+        round_df = get_round_results(season_data, i)
+        if i == prediction_round-round_wdw:
+            all_qualis_df = round_df
+            break # Prueba para los datos de la primera ronda
+        else:
+            all_qualis_df = pd.concat([all_qualis_df, round_df], axis=0)
+    print('Data from all rounds concatenated!')
+    
+print('Preprocessing all rounds data...')
+qualis_df = preprocess_round_data(all_qualis_df)
+print('Preprocessing successful')
